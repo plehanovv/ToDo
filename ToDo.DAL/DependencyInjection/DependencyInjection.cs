610d0cf1e2b715a6532ad9701979e6646dc65898
yarik <plehanov.kzn2@gmail.com>
2025-01-21 20:01:55 +0300

    configured DAL

---
+++ ToDo.DAL/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ToDo.DAL.Interceptors;
+using ToDo.DAL.Repositories;
+using ToDo.Domain.Entity;
+using ToDo.Domain.Interfaces.Repositories;
+
+namespace ToDo.DAL.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("MSSQL");
+
+        services.AddSingleton<DateInterceptor>();
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+        
+        
+    }
+
+    public static void InitRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IBaseRepository<User>, BaseRepository<User>>();
+        services.AddScoped<IBaseRepository<Report>, BaseRepository<Report>>();
+    }
+}
