c87c82fae1fbf7eaa8ead458bece53048cc70092
yaroslav <plehanov.kzn2@gmail.com>
2025-01-27 19:15:58 +0300

    В IReportService добавил методы CRUD операций

--- ToDo.Domain/Interfaces/Services/IReportService.cs
+++ ToDo.Domain/Interfaces/Services/IReportService.cs
@@ -1,4 +1,4 @@
-using ToDo.Domain.Dto;
+using ToDo.Domain.Dto.Report;
 using ToDo.Domain.Result;
 
 namespace ToDo.Domain.Interfaces.Services;
@@ -14,4 +14,32 @@ public interface IReportService
     /// <param name="userId"></param>
     /// <returns></returns>
     Task<CollectionResult<ReportDto>> GetReportsAsync(long userId);
+    
+    /// <summary>
+    /// Получение отчета по id 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<BaseResult<ReportDto>> GetReportByIdAsync(long id);
+    
+    /// <summary>
+    /// Создание нового отчета с базовыми параметрами
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    Task<BaseResult<ReportDto>> CreateReportAsync(CreateReportDto dto);
+    
+    /// <summary>
+    /// Удаление отчета по id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    Task<BaseResult<ReportDto>> DeleteReportAsync(long id);
+    
+    /// <summary>
+    /// Обновление отчета по id
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns></returns>
+    Task<BaseResult<ReportDto>> UpdateReportAsync(UpdateReportDto dto);
 }
