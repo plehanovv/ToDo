8bdba4c941b4953ccf1a0434ff2e0b3146946e8c
yaroslav <plehanov.kzn2@gmail.com>
2025-01-28 20:46:13 +0300

    Добавил контроллер, запись логов, подключение зависимостей в точку входа программы

--- ToDo.Api/Program.cs
+++ ToDo.Api/Program.cs
@@ -1,7 +1,17 @@
+using Serilog;
+using ToDo.Application.DependencyInjection;
+using ToDo.DAL.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddDataAccessLayer(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
@@ -12,4 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
+
 app.Run();
