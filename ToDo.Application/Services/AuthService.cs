bdb206941b5907e7ef0fc72b13fdddad519ac618
yaroslav <plehanov.kzn2@gmail.com>
2025-01-30 18:51:58 +0300

    Написал сервис и контроллер с регистрацией

---
+++ ToDo.Application/Services/AuthService.cs
@@ -0,0 +1,89 @@
+using System.Security.Cryptography;
+using System.Text;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using ToDo.Application.Resources;
+using ToDo.Domain.Dto;
+using ToDo.Domain.Dto.User;
+using ToDo.Domain.Entity;
+using ToDo.Domain.Enum;
+using ToDo.Domain.Interfaces.Repositories;
+using ToDo.Domain.Interfaces.Services;
+using ToDo.Domain.Result;
+
+namespace ToDo.Application.Services;
+
+public class AuthService : IAuthService
+{
+    private readonly IBaseRepository<User> _userRepository;
+    private readonly ILogger _logger;
+    private readonly IMapper _mapper;
+
+    public AuthService(IBaseRepository<User> userRepository, ILogger logger, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _logger = logger;
+        _mapper = mapper;
+    }
+
+    public async Task<BaseResult<UserDto>> Register(RegisterUserDto dto)
+    {
+        if (dto.Password != dto.PasswordConfirm)
+        {
+            return new BaseResult<UserDto>()
+            {
+                ErrorMessage = ErrorMessage.PasswordNotEqualsPasswordConfirm,
+                ErrorCode = (int)ErrorCodes.PasswordNotEqualsPasswordConfirm
+            };
+        }
+
+        try
+        {
+            var user = await _userRepository.GetAll().FirstOrDefaultAsync(x => x.Login == dto.Login);
+            if (user != null)
+            {
+                return new BaseResult<UserDto>()
+                {
+                    ErrorMessage = ErrorMessage.UserAlreadyExists,
+                    ErrorCode = (int)ErrorCodes.UserAlreadyExists
+                    
+                };
+            }
+            
+            var hashUserPassword = HashPassword(dto.Password);
+            user = new User()
+            {
+                Login = dto.Login,
+                Password = hashUserPassword,
+            };
+
+            await _userRepository.CreateAsync(user);
+
+            return new BaseResult<UserDto>()
+            {
+                Data = _mapper.Map<UserDto>(user)
+            };
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, ex.Message);
+            return new BaseResult<UserDto>()
+            {
+                ErrorMessage = ErrorMessage.InternalServerError,
+                ErrorCode = (int)ErrorCodes.InternalServerError
+            };
+        }
+    }
+
+    public Task<BaseResult<TokenDto>> Login(LoginUserDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    private string HashPassword(string password)
+    {
+        var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(password));
+        return BitConverter.ToString(bytes).ToLower();
+    }
+}
