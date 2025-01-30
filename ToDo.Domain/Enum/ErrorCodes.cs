98343f3adaf26890f98624c3c2f1cfa52be5f5ac
yaroslav <plehanov.kzn2@gmail.com>
2025-01-30 18:50:51 +0300

    Добавил новые ошибки

--- ToDo.Domain/Enum/ErrorCodes.cs
+++ ToDo.Domain/Enum/ErrorCodes.cs
@@ -2,11 +2,17 @@ namespace ToDo.Domain.Enum;
 
 public enum ErrorCodes
 {
+    // 0-10 Отчеты
     ReportsNotFound = 0,
     ReportNotFound = 1,
     ReportAlreadyExists = 2,
     
     InternalServerError = 10,
     
+    // 11-20 Пользоваель
     UserNotFound = 11,
+    UserAlreadyExists = 12,
+    
+    // 21-30 Авторизация и регистрация
+    PasswordNotEqualsPasswordConfirm = 21,
 }
